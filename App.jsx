a8fabedc6b4985f2ffc5ab60a2bdfd6bd542f8bc
Shibasis <shibasis0801@gmail.com>
2026-01-30 03:10:48 +0530

    content ok, styling 400

--- App.jsx
+++ App.jsx
@@ -1,187 +1,186 @@
 import React from 'react';
 
-const Skill = ({ title, skills }) => (
+const Domain = ({ title, skills }) => (
     <tr>
-      <td className="left">
-        <h4>{title}: </h4>
-      </td>
-      <td className="right">
-        {skills.map(skill => (
-            <span key={skill}>{skill}</span>
-        ))}
-      </td>
+        <td className="left">
+            <h4>{title} </h4>
+        </td>
+        <td className="right">
+            {skills.map(skill => (
+                <span key={skill}>{skill}</span>
+            ))}
+        </td>
     </tr>
 )
 
+const Experience = ({ duration, children }) => (
+    <tr>
+        <td className="left">
+            <h4>{duration}</h4>
+        </td>
+        <td className="right">
+            <h4>{children}</h4>
+        </td>
+    </tr>
+)
+
+const Category = ({ title, children }) => (
+    <section>
+        <h4><strong>{title}</strong></h4>
+        <ul>
+            {children}
+        </ul>
+    </section>
+)
+
+const Project = ({ title, link, children }) => (
+    <li>
+        <strong><a href={link}>{title}: </a></strong>
+        {children}
+    </li>
+)
+
 function App() {
-  return (
-    <div>
-        <div className="header">
-            <h1>SHIBASIS PATNAIK</h1>
-            <span>
-                <a href="https://github.com/shibasis0801">GitHub</a>
-                <a href="https://shibasis-patnaik.medium.com/">Medium</a>
-                <a href="https://www.linkedin.com/in/shibasis-patnaik-8010a5127/">LinkedIn</a>
+    return (
+        <div>
+            <div className="header">
+                <h1>SHIBASIS PATNAIK</h1>
+                <span>
+                <a style={{ margin: "16px" }} href="https://github.com/shibasis0801">GitHub</a>
+                <a style={{ margin: "16px" }} href="https://shibasis-patnaik.medium.com/">Medium</a>
+                <a style={{ margin: "16px" }} href="https://www.linkedin.com/in/shibasis-patnaik-8010a5127/">LinkedIn</a>
             </span>
-        </div>
+            </div>
 
-        <p>
-            Passionate developer with deep expertise in mobile, cross-platform and micro-services. I love computers and always try to creatively connect things. Currently learning foundational AI and computational neuroscience.
-        </p>
+            <p>
+                Passionate developer with deep expertise in mobile, cross-platform and micro-services.
+                <br/> I love computers and always try to creatively connect things.
+            </p>
 
-        <section>
-            <section>
-                <h2>EDUCATION</h2>
-                <h4> B.Tech in <strong>Computer Science</strong>,<strong>National Institute Of Technology, Rourkela</strong> (2019) </h4>
+
+
+            <h2>WORK EXPERIENCE</h2>
+            <table>
+                <tbody>
+                <Experience duration="Oct '21 - Present">
+                    <strong>Senior Software Engineer, Myntra</strong>
+                </Experience>
+                <Experience duration="Jul '19 - Oct '21">
+                    Software Engineer, Myntra
+                </Experience>
+                </tbody>
+            </table>
+
+            <h2>SKILLS</h2>
+            <table>
+                <tbody>
+                <Domain title="Languages"
+                        skills={["C++", "Kotlin", "Java", "TypeScript", "JavaScript", "Python"]}
+                />
+                <Domain title="Mobile"
+                        skills={["Android", "Web", "Coroutines", "GCD", "fbJNI", "Koin", "SQLDelight"]}
+                />
+                <Domain title="Backend"
+                        skills={["Spring Boot", "Express.js", "Kafka", "Hibernate", "AOP", "Kubernetes"]}
+                />
+                <Domain title="Databases"
+                        skills={["PostgreSQL", "MySQL", "SQLite", "TiDB", "Memgraph"]}
+                />
+                <Domain title="Runtimes & Internals"
+                        skills={["Kotlin", "JVM", "Node.js", "React Native/JSI", "FlatBuffers"]}
+                />
+                <Domain title="Kotlin Multiplatform"
+                        skills={["Compose Multiplatform", "cinterop", "karakum", "CMake"]}
+                />
+                <Domain title="React"
+                        skills={["React Native", "React", "JSI", "Hermes"]}
+                />
+                <Domain title="Cloud"
+                        skills={["Cloudflare Actors, Workers", "GCP Cloud Run, Compute Engine, Firebase"]}
+                />
+                <Domain title="Tooling"
+                        skills={["Gradle Plugins", "Custom Serializers", "Profiling", "FlameCharts", "Grafana"]}
+                />
+                <Domain title="AI"
+                        skills={["MLKit", "TensorFlow", "Keras", "Scikit-Learn"]}
+                />
+                </tbody>
+            </table>
+
+            <h2>EXPERIENCE</h2>
+            <div>
+                <Category title="Product">
+                    <li>
+                        Built flows to enable <strong>Myntra Hyperlocal</strong> orders on Flipkart WMS. Added Customer
+                        and Non-Customer outbound flows.
+                    </li>
+                    <li>
+                        Wrote a <strong>AuthNZ</strong> library for use in multiple Flipkart WMS services using Spring AOP for single line integration.
+                    </li>
+                    <li>
+                        Led a team of 4 and migrated <strong>Size Selector</strong> from client to server, this drives the Myntra Product page
+                    </li>
+                    <li>
+                        Built <strong>Rebates</strong> and Incentive flows for Myntra Partners among other features. Spring, Express, Kafka.
+                    </li>
+                    <li>
+                        Led a team of 2 to build the <strong>Mobile Number Login </strong> and Recovery flows for Myntra.
+                    </li>
+                </Category>
+                <Category title="Systems">
+                    <li>
+                        Wrote a <strong>C++ framework (Reaktor)</strong> on JSI for Android and iOS for interfacing Kotlin Multiplatform and React Native.
+                    </li>
+                    <li>
+                        Led a team of 5 to build the <strong>Database module</strong> using Reaktor for storing layout-engine responses.
+                    </li>
+                </Category>
+
+                <Category title="Performance">
+                    <li>
+                        Fixed Jetty and ThreadPool misconfigurations. Reduced 2000 threads <strong>saving 2GB </strong>of native memory per pod.
+                    </li>
+                    <li>
+                        Analysed Flipkart Reservation services and databases (TiDB) for <strong> excessive GC, memory leaks, slow queries </strong>.
+                    </li>
+
+                    <li>
+                        Added <strong>automated dashboard generation</strong> based on server introspection and Grafana observability for Express.js.
+                    </li>
+                </Category>
+
+            </div>
+
+            <h2>PROJECTS</h2>
+            <div>
+                <strong><a href="https://github.com/shibasis0801/flatInvoker/tree/main/reaktor-graph/src/commonMain/kotlin/dev/shibasis/reaktor/graph/core">
+                    Reaktor:
+                </a></strong>
+                {" "} Full stack (android, iOS, web, c++, node, spring) Kotlin multiplatform modules and services for next generation products.
                 <ul>
-                    <li>Led institute team to <strong>National Finals</strong> at Smart India Hackathon 2017 by Government of India.</li>
-                    <li>Mentored classmates and juniors. Built apps for NIT Rourkela and NIT Warangal.</li>
+                    <li>PortGraph for <strong>Flow based programming</strong> </li>
+                    <li>tRPC style REST APIs without codegen </li>
+                    <li>SQL based RBAC JWT Auth with Apple/Google login. </li>
+                    <li>Object Database using Kotlin Serialization </li>
+                    <li>Google/Flexbuffers for language independent structs. </li>
                 </ul>
-            </section>
-            <section>
-                <h2>WORK EXPERIENCE</h2>
-                <table>
-                    <tbody>
-                        <tr>
-                            <td className="left">
-                                <h4>Oct '21 - Present</h4>
-                            </td>
-                            <td className="right">
-                                <h4><strong>Senior Software Engineer, Myntra</strong></h4>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="left">
-                                <h4>Jul '19 - Oct '21</h4>
-                            </td>
-                            <td className="right">
-                                <h4>Software Engineer, Myntra</h4>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="left">
-                                <h4>May '17 - Sept '17</h4>
-                            </td>
-                            <td className="right">
-                                <h4>Software Engineering Intern, Awalk</h4>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
+                <Category title="Product">
+                    <Project title="BestBuds" link="https://bestbuds.ai/">
+                        Meet new groups of friends to take part in activities together. Built with Reaktor.
+                    </Project>
+                    <Project title="Manna" link="https://manna.ac/">
+                        Create roadmaps and schedules to track your progress. Built with Reaktor.
+                    </Project>
+                </Category>
+            </div>
 
-                <h2>SYSTEMS</h2>
-                <h3>SKILLS</h3>
-                <table>
-                    <tbody>
-                        <Skill title="Languages" skills={["C++", "Kotlin", "Python", "Objective-C"]} />
-                        <Skill title="AI" skills={["MLKit", "TensorFlow", "Keras", "Scikit-Learn", "Spark", "MediaPipe"]} />
-                        <Skill title="Build Tools" skills={["Gradle Plugin", "Kotlin Serialization Custom Formats", "KSP Plugins"]} />
-                        <Skill title="Frameworks" skills={["Kotlin Multiplatform", "iOS", "SQLDelight", "Ktor", "Kotlin Serialization"]} />
-                    </tbody>
-                </table>
-
-                <h3>EXPERIENCE</h3>
-                <div>
-                    <ul>
-                        <li>
-                            <strong>Myntra, 2023:</strong> Wrote a <strong>C++ framework (Reaktor)</strong> on JSI for interfacing Kotlin Multiplatform and React Native,
-                            functionally more powerful than Facebook’s Turbo Modules.
-                        </li>
-                        <li>
-                            <strong>Myntra, 2023:</strong> Led a team of 5 to build the <strong>Database module</strong> using Reaktor for storing layout-engine responses (myntra’s server-driven framework).
-                        </li>
-                    </ul>
-                </div>
-
-                <h3>PROJECTS</h3>
-                <div>
-                    <ul>
-                        <li>
-                            <strong>FlatInvoker, 2024: </strong> Highly improved redesign of Reaktor.
-                            Leveraging Google's <strong>FlexBuffers</strong> as the underlying memory layout.
-                            Trying to optimise cache behaviour by moving away from reference containing classes to value containing structs in Kotlin Multiplatform.
-                        </li>
-                        <li>
-                            <strong>Manna, 2022-Present:</strong>. Learning applied AI by building a RAG using <strong>pg_vector</strong> on postgreSQL(Supabase) in order to query notes created on the app and get answers.
-                        </li>
-                    </ul>
-                </div>
-            </section>
-
-            <h2>MOBILE</h2>
-            <h3>SKILLS</h3>
-            <table>
-                <tbody>
-                    <Skill title="Languages" skills={["Kotlin", "C++", "TypeScript", "JavaScript"]} />
-                    <Skill title="Android" skills={["JNI", "Coroutines", "Custom Views", "MLKit", "MediaPipe"]} />
-                    <Skill title="React" skills={["JSI", "React Native", "Server Components", "Custom Hooks"]} />
-                    <Skill title="Compose" skills={["Custom Navigation", "Multiplatform Adapters", "Image Caching"]} />
-                    <Skill title="Web" skills={["PWA", "Differential Bundling", "Lazy Loading", "HTML, CSS", "SASS"]} />
-                </tbody>
-            </table>
-
-            <h3>EXPERIENCE</h3>
+            <h2>EDUCATION</h2>
+            <h4> B.Tech in <strong>Computer Science</strong>, <strong>National Institute Of Technology, Rourkela</strong> (2019) </h4>
             <ul>
-                <li>
-                    <strong>Myntra, 2023:</strong> Led a team of 4 to build the <strong>Size Selector</strong> feature on Product Page with our <strong>server driven UI framework.</strong>
-                    Size Selector is the heart of Myntra's product page and all other features depend on it.
-                </li>
-                <li>
-                    <strong>Myntra, 2021:</strong>Built <strong>Mobile Login and Recovery flow</strong> for Myntra mobile and desktop websites. Led a team of 2 in recovery flow design and
-                    development and delivered on a tight schedule.
-                </li>
-                <li>
-                    <strong>Myntra, 2021:</strong>Built <strong>delivery pincode, address selection and location context personalization</strong> for web with Cypress end-to-end <strong>automated
-                    tests.</strong>
-                </li>
+                <li>Led institute team to <strong>National Finals</strong> at Smart India Hackathon 2017 by Government of India.</li>
             </ul>
-
-            <h3>PROJECTS</h3>
-            <ul>
-                <li>
-                    <strong>Mehmaan, 2022-Present:</strong> Building a product for managing weddings. Heavily uses <strong>Kotlin Multiplatform and Jetpack Compose</strong>.
-                </li>
-                <li>
-                    <strong>Manna, 2022-Present:</strong> Building a <strong>spaced repetition</strong> for notes and mental map sharing app. Building it on GCP with user’s Google Drive for privacy
-                    and storage efficiency.
-                </li>
-            </ul>
-        </section>
-        <section>
-            <h2>BACK-END</h2>
-            <h3>SKILLS</h3>
-            <table>
-                <tbody>
-                    <Skill title="Languages" skills={["Java", "Kotlin", "Python", "TypeScript"]} />
-                    <Skill title="Frameworks" skills={["Vert.x", "Spring Boot", "Express.js", "Flask"]} />
-                    <Skill title="Data Layer" skills={["PostgreSQL", "MySQL", "SQLite", "Kafka", "AirFlow"]} />
-                    <Skill title="Areas" skills={["Event Driven", "Auth and JWT", "App proxy servers"]} />
-                    <Skill title="Cloud" skills={["Cloudflare D1, R2, Workers", "GCP Cloud Run", "Firebase", "Supabase"]} />
-                </tbody>
-            </table>
-
-
-            <h3>EXPERIENCE</h3>
-            <ul>
-                <li>
-                    <strong>Myntra, 2024</strong> Working on <strong>event driven microservices</strong> in spring boot, for multiple features involving MySQL and Kafka.
-                    Building Seller Protection Fund Automation in a team of 2.
-                </li>
-                <li>
-                    <strong>Myntra, 2023:</strong> For <strong>Size Selector</strong>, analysed highly complex distributed logic in the app and rewrote <strong>complete winner seller logic</strong>.
-                    As we were in crunch time, contributed significantly to the backend.
-                </li>
-            </ul>
-
-            <h3>PROJECTS</h3>
-            <ul>
-                <li>
-                    <strong>Mehmaan, 2022-Present:</strong> Writing a role based auth server for dynamic feature delivery on the mobile app according to customer's subscription.
-                </li>
-            </ul>
-        </section>
-    </div>
-  );
+        </div>
+    );
 }
 
 export default App;
